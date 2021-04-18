9132b71fe26f60f40a6a2dae186475d9ae12fb7e
Julie Branyan <juliebranyan@gmail.com>
2021-04-17 21:24:46 -0700

    Focus on name field and Job Role other displays on selection

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('pizza');
+/*
+Treehouse Techdegree:
+FSJS Project 3 - Interactive Form
+Author: Julie Branyan
+*/
+const jobRole = document.getElementById("title");
+const otherJobRoleDiv = document.querySelector(".otherJobRole");
+document.getElementById("name").focus();
+otherJobRoleDiv.style.display = 'none';
+
+
+jobRole.addEventListener('click', () =>{
+  console.log(jobRole.value);
+  if(jobRole.value == 'other'){
+    if(otherJobRoleDiv.style.display == 'none'){
+      otherJobRoleDiv.style.display = 'block';  
+    } 
+  } else {
+    otherJobRoleDiv.style.display = 'none'
+  }
+});
