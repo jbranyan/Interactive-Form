6b10ac1d8d517ea8cfaf4c46a3d5912bde0b6f21
Julie Branyan <juliebranyan@gmail.com>
2021-04-21 07:07:26 -0700

    T-shirt info section displays colors upon sselection

--- js/script.js
+++ js/script.js
@@ -5,13 +5,21 @@ Author: Julie Branyan
 */
 const jobRole = document.getElementById("title");
 const otherJobRoleDiv = document.querySelector(".otherJobRole");
+const shirtDesign = document.getElementById("design");
+const shirtColor = document.querySelector(".shirt-colors");
+const color = document.querySelector("#color");
+
+console.log(shirtColor);
+console.log(shirtDesign);
+console.log(color);
+
 document.getElementById("name").focus();
 otherJobRoleDiv.style.display = 'none';
 
 
-jobRole.addEventListener('click', () =>{
+jobRole.addEventListener('change', e =>{
   console.log(jobRole.value);
-  if(jobRole.value == 'other'){
+  if(e.target.value == 'other'){
     if(otherJobRoleDiv.style.display == 'none'){
       otherJobRoleDiv.style.display = 'block';  
     } 
@@ -19,3 +27,24 @@ jobRole.addEventListener('click', () =>{
     otherJobRoleDiv.style.display = 'none'
   }
 });
+
+shirtDesign.addEventListener('change', e =>{
+  
+  shirtColor.style.display = 'block';
+  console.log(color);
+
+  for(let i=0; i < color.length; i++){
+
+    const colorsToDisplay = color[i].getAttribute("data-theme");
+    const shirtSelected = e.target.value;
+
+    if(shirtSelected === colorsToDisplay){
+      color[i].hidden = false;
+      console.log(colorsToDisplay);
+    } else {
+      color[i].hidden = true;
+      console.log(colorsToDisplay);
+    }
+  } 
+});
+
